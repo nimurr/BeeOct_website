748567845dd4d83d4effec820f1195992be5a0b6
nimurr <nimurnerob404@gmail.com>
2025-12-23 22:27:58 +0600

    `Added background images and updated layout for Home/AllTechStacks, Home/OurIndustryExpertise, and Home/Services components`

--- src/Components/Home/OurIndustryExpertise.js
+++ src/Components/Home/OurIndustryExpertise.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const OurIndustryExpertise = () => {
     return (
-        <div>
-            <h2>Our Industry Expertise</h2>
+        <div className='relative min-h-[50vh] flex items-center'>
+            <img className=' absolute max-w-[600px] bottom-1/2 lg:bottom-0 left-0' src="/Images/Home/partners/ourIndustry.png" alt="" />
+            <div className='container  mx-auto px-2 lg:px-4'>
+                <div className="text-center mb-10 lg:mb-20">
+                    <h2 className="text-4xl font-semibold">Our Industry Expertise</h2>
+                    <p className="text-gray-400 mt-2">
+                        We serve diverse industries with specialized solutions
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
